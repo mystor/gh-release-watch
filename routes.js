7c932b3909b2873b11e68ebc1fb181288fae9dcb
Michael Layzell <michael@thelayzells.com>
2014-09-09 15:54:21 -0400

    add google analytics

--- routes.js
+++ routes.js
@@ -1,5 +1,16 @@
 Routes = {};
 
+if (Meteor.isClient) {
+  // Load google analytics
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+  ga('create', 'UA-54612644-1', 'auto');
+  ga('send', 'pageview'); // TODO: Better tracking
+}
+
 RouteCore.map(function () {
   Routes.home = this.route('/', function (ctx) {
     // Make sure that the user is sent down to the client
@@ -75,4 +86,3 @@ RouteCore.map(function () {
       }, NotFound(null));
     });
 });
-
